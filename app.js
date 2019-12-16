1e6349f59517e7ed351afaa28b9a3adf72f1dcc5
apuyina <florencia.quatrin@gmail.com>
2019-12-15 22:38:04 -0300

    Cuarto intento

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var exphbs=require('express-handlebars');
 var app=express();
-var puerto=process.env.port || 3000;
+var puerto= process.env.PORT || 3000;
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb+srv://maira:bob@cluster0-tioej.mongodb.net/test?retryWrites=true&w=majority'
 // Database Name
